397307f304fc15850ca87c7e4239ec53c8779d02
masete <nicholasmasete72@gmail.com>
2021-03-13 14:04:53 +0300

    added home templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 import datetime
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 from flask_babelex import Babel
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
 from app.config import ConfigClass
-
+# from app.models import User
 # Class-based application configuration
 
 
@@ -23,80 +23,11 @@ def create_app():
 
     # Define the User data-model.
     # NB: Make sure to add flask_user UserMixin !!!
-    class User(db.Model, UserMixin):
-        __tablename__ = 'users'
-        id = db.Column(db.Integer, primary_key=True)
-        active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
-
-        # User authentication information. The collation='NOCASE' is required
-        # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
-        email = db.Column(db.String(255), nullable=False, unique=True)
-        email_confirmed_at = db.Column(db.DateTime())
-        password = db.Column(db.String(255), nullable=False, server_default='')
-
-        # User information
-        first_name = db.Column(db.String(100), nullable=False, server_default='')
-        last_name = db.Column(db.String(100), nullable=False, server_default='')
-
-        # Define the relationship to Role via UserRoles
-        roles = db.relationship('Role', secondary='user_roles')
-
-    # Define the Role data-model
-    class Role(db.Model):
-        __tablename__ = 'roles'
-        id = db.Column(db.Integer(), primary_key=True)
-        name = db.Column(db.String(50), unique=True)
-
-    # Define the UserRoles association table
-    class UserRoles(db.Model):
-        __tablename__ = 'user_roles'
-        id = db.Column(db.Integer(), primary_key=True)
-        user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
-        role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
-
-    # Setup Flask-User and specify the User data-model
-    user_manager = UserManager(app, db, User)
-
-    # Create all database tables
-    db.create_all()
-
-    # Create 'member@example.com' user with no roles
-    if not User.query.filter(User.email == 'member@example.com').first():
-        user = User(
-            email='member@example.com',
-            email_confirmed_at=datetime.datetime.utcnow(),
-            password=user_manager.hash_password('Password1'),
-        )
-        db.session.add(user)
-        db.session.commit()
-
-    # Create 'admin@example.com' user with 'Admin' and 'Agent' roles
-    if not User.query.filter(User.email == 'admin@example.com').first():
-        user = User(
-            email='admin@example.com',
-            email_confirmed_at=datetime.datetime.utcnow(),
-            password=user_manager.hash_password('Password1'),
-        )
-        user.roles.append(Role(name='Admin'))
-        user.roles.append(Role(name='Agent'))
-        db.session.add(user)
-        db.session.commit()
-
+   
     # The Home page is accessible to anyone
-    @app.route('/')
+    @app.route('/app/templates/home')
     def home_page():
-        return render_template_string("""
-                {% extends "flask_user_layout.html" %}
-                {% block content %}
-                    <h2>{%trans%}Home page{%endtrans%}</h2>
-                    <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-                    <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-                    <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-                    <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
-                    <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
-                    <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-                {% endblock %}
-                """)
+        return render_template('home')
 
     # The Members page is only accessible to authenticated users
     @app.route('/members')
