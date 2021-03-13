40f38f9a9f73a33895c97832e2a83ab3e215b550
masete <nicholasmasete72@gmail.com>
2021-03-13 15:48:44 +0300

    added missing templates

--- app/__init__.py
+++ app/__init__.py
@@ -28,40 +28,31 @@ def create_app():
     @app.route('/')
     @app.route('/home_page', methods=['POST', 'GET'])
     def home_page():
-        return render_template('home_page.html')
+        return render_template("home_page.html")
+
+    
+    @app.route('/register', methods=['POST', 'GET'])
+    def register():
+        return render_template("register.html")
+
+    @app.route('/login', methods=['POST', 'GET'])
+    def login():
+        return render_template("login.html")
+
+    @app.route('/logout', methods=['POST', 'GET'])
+    def logout():
+        return render_template("logout.html")
 
     # The Members page is only accessible to authenticated users
-    # @app.route('/members')
-    # @login_required    # Use of @login_required decorator
-    # def member_page():
-    #     return render_template_string("""
-    #             {% extends "flask_user_layout.html" %}
-    #             {% block content %}
-    #                 <h2>{%trans%}Members page{%endtrans%}</h2>
-    #                 <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-    #                 <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-    #                 <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-    #                 <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
-    #                 <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
-    #                 <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-    #             {% endblock %}
-    #             """)
+    @app.route('/members')
+    @login_required    # Use of @login_required decorator
+    def member_page():
+        return render_template('member_page')
 
-    # # The Admin page requires an 'Admin' role.
-    # @app.route('/admin')
-    # @roles_required('Admin')    # Use of @roles_required decorator
-    # def admin_page():
-    #     return render_template_string("""
-    #             {% extends "flask_user_layout.html" %}
-    #             {% block content %}
-    #                 <h2>{%trans%}Admin Page{%endtrans%}</h2>
-    #                 <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-    #                 <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-    #                 <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-    #                 <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
-    #                 <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
-    #                 <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-    #             {% endblock %}
-    #             """)
+    # The Admin page requires an 'Admin' role.
+    @app.route('/admin')
+    @roles_required('Admin')    # Use of @roles_required decorator
+    def admin_page():
+        return render_template('admin_page')
 
     return app
