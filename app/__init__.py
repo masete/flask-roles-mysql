9b2ef127123b8b2eda967185d6a931d52674033f
masete <nicholasmasete72@gmail.com>
2021-03-13 13:45:00 +0300

    added the config file

--- app/__init__.py
+++ app/__init__.py
@@ -3,34 +3,9 @@ from flask import Flask, request, render_template_string
 from flask_babelex import Babel
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
-
+from app.config import ConfigClass
 
 # Class-based application configuration
-class ConfigClass(object):
-    """ Flask application config """
-
-    # Flask settings
-    SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
-
-    # Flask-SQLAlchemy settings
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:13579246@localhost/flaskdashboard'    # File-based SQL database
-    SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
-
-    # Flask-Mail SMTP server settings
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = False
-    MAIL_USERNAME = 'email@example.com'
-    MAIL_PASSWORD = 'password'
-    MAIL_DEFAULT_SENDER = '"MyApp" <noreply@example.com>'
-
-    # Flask-User settings
-    USER_APP_NAME = "Flask-User Basic App"      # Shown in and email templates and page footers
-    USER_ENABLE_EMAIL = True        # Enable email authentication
-    USER_ENABLE_USERNAME = False    # Disable username authentication
-    USER_EMAIL_SENDER_NAME = USER_APP_NAME
-    USER_EMAIL_SENDER_EMAIL = "noreply@example.com"
 
 
 def create_app():
